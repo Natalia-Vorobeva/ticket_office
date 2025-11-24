04a924af194072fa6c3d45da66e37b871798206f
Natalia <vorobjeva.natalia76@gmail.com>
2025-11-25 03:33:39 +0500

    constants

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "https://natalia-vorobeva.github.io/ticket_office/",
   plugins: [react()],
 })
